e1a43ecfefddf85ad7bad13d693aeefb94b65b96
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2020-03-29 00:01:10 +0000

    feat: integrate the register form with the backend

--- www/.eslintrc.js
+++ www/.eslintrc.js
@@ -21,6 +21,9 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint'],
-  rules: {},
+  plugins: ['react', '@typescript-eslint', 'react-hooks'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
 }
--- www/src/pages/Register/index.tsx
+++ www/src/pages/Register/index.tsx
@@ -1,12 +1,89 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useCallback, useReducer } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
 
 import logoImg from '../../assets/logo.svg'
 
+import api from '../../services/api'
+
 import './styles.css'
 
+type State = {
+  name: string
+  email: string
+  whatsapp: string
+  city: string
+  uf: string
+}
+
+type Action =
+  | { type: 'SET_NAME'; payload: string }
+  | { type: 'SET_EMAIL'; payload: string }
+  | { type: 'SET_WHATSAPP'; payload: string }
+  | { type: 'SET_CITY'; payload: string }
+  | { type: 'SET_UF'; payload: string }
+
+const initialState: State = {
+  name: '',
+  email: '',
+  whatsapp: '',
+  city: '',
+  uf: '',
+}
+
+function reducer(state: State, action: Action) {
+  switch (action.type) {
+    case 'SET_NAME':
+      return {
+        ...state,
+        name: action.payload,
+      }
+    case 'SET_EMAIL':
+      return {
+        ...state,
+        email: action.payload,
+      }
+    case 'SET_WHATSAPP':
+      return {
+        ...state,
+        whatsapp: action.payload,
+      }
+    case 'SET_CITY':
+      return {
+        ...state,
+        city: action.payload,
+      }
+    case 'SET_UF':
+      return {
+        ...state,
+        uf: action.payload,
+      }
+    default:
+      return state
+  }
+}
+
 function Register() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const history = useHistory()
+
+  const handleRegister = useCallback(
+    async (event) => {
+      event.preventDefault()
+
+      try {
+        const response = await api.post('ongs', state)
+
+        alert(`Seu ID de acesso: ${response.data.id}`)
+
+        history.push('/')
+      } catch (err) {
+        alert(`Erro no cadastro! Tente novamente`)
+      }
+    },
+    [state, history]
+  )
+
   return (
     <div className="register-container">
       <div className="content">
@@ -25,14 +102,50 @@ function Register() {
           </Link>
         </section>
 
-        <form action="submit">
-          <input type="text" placeholder="Nome da ONG" />
-          <input type="email" placeholder="E-mail" />
-          <input type="text" placeholder="WhatsApp" />
+        <form action="submit" onSubmit={handleRegister}>
+          <input
+            type="text"
+            placeholder="Nome da ONG"
+            value={state.name}
+            onChange={(e) =>
+              dispatch({ type: 'SET_NAME', payload: e.target.value })
+            }
+          />
+          <input
+            type="email"
+            placeholder="E-mail"
+            value={state.email}
+            onChange={(e) =>
+              dispatch({ type: 'SET_EMAIL', payload: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            placeholder="WhatsApp"
+            value={state.whatsapp}
+            onChange={(e) =>
+              dispatch({ type: 'SET_WHATSAPP', payload: e.target.value })
+            }
+          />
 
           <div className="input-group">
-            <input type="text" placeholder="Cidade" />
-            <input type="text" placeholder="UF" style={{ width: 80 }} />
+            <input
+              type="text"
+              placeholder="Cidade"
+              value={state.city}
+              onChange={(e) =>
+                dispatch({ type: 'SET_CITY', payload: e.target.value })
+              }
+            />
+            <input
+              type="text"
+              placeholder="UF"
+              value={state.uf}
+              style={{ width: 80 }}
+              onChange={(e) =>
+                dispatch({ type: 'SET_UF', payload: e.target.value })
+              }
+            />
           </div>
 
           <button className="button" type="submit">
