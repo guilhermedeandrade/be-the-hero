9451b9eae7f80f3af9bb8005d801cde0639600d0
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2020-03-29 02:58:36 +0100

    feat: integrate Profile listing with the backend

--- www/src/pages/Profile/index.tsx
+++ www/src/pages/Profile/index.tsx
@@ -1,23 +1,76 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState, useCallback } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import { FiPower, FiTrash2 } from 'react-icons/fi'
+
+import api from '../../services/api'
 
 import logoImg from '../../assets/logo.svg'
 
 import './styles.css'
 
-export default function Profile() {
+type Incident = {
+  id: number
+  title: string
+  description: string
+  value: number
+  ongId: string
+}
+
+type State = Incident[]
+
+function Profile() {
+  const [incidents, setIncidents] = useState<State>([])
+  const history = useHistory()
+
+  const ongName = localStorage.getItem('ongName')
+  const ongId = localStorage.getItem('ongId')
+
+  useEffect(() => {
+    api
+      .get('profile', {
+        headers: {
+          Authorization: ongId,
+        },
+      })
+      .then((res) => {
+        setIncidents(res.data)
+      })
+  }, [ongId])
+
+  const handleDeleteIncident = useCallback(
+    async (id) => {
+      try {
+        await api.delete(`incidents/${id}`, {
+          headers: {
+            Authorization: ongId,
+          },
+        })
+
+        setIncidents(incidents.filter((incident) => incident.id !== id))
+      } catch (err) {
+        alert('Erro ao deletar caso, tente novamente')
+      }
+    },
+    [ongId, setIncidents, incidents]
+  )
+
+  const handleLogout = useCallback(() => {
+    localStorage.clear()
+
+    history.push('/')
+  }, [history])
+
   return (
     <div className="profile-container">
       <header>
         <img src={logoImg} alt="Be The Hero" />
-        <span>Bem vinda, ONG</span>
+        <span>Bem vinda, {ongName}</span>
 
         <Link to="/incidents/new" className="button">
           Cadastrar novo caso
         </Link>
 
-        <button type="button">
+        <button type="button" onClick={handleLogout}>
           <FiPower size={18} color="#e02041" />
         </button>
       </header>
@@ -25,83 +78,33 @@ export default function Profile() {
       <h1>Casos cadastrados</h1>
 
       <ul>
-        <li>
-          <strong>CASO:</strong>
-          <p>Nome do caso</p>
+        {incidents.map((incident) => (
+          <li key={incident.id}>
+            <strong>CASO:</strong>
+            <p>{incident.title}</p>
 
-          <strong>DESCRIÇÃO:</strong>
-          <p>Descrição do caso</p>
+            <strong>DESCRIÇÃO:</strong>
+            <p>{incident.description}</p>
 
-          <strong>VALOR:</strong>
-          <p>
-            {Intl.NumberFormat('pt-br', {
-              style: 'currency',
-              currency: 'BRL',
-            }).format(777)}
-          </p>
+            <strong>VALOR:</strong>
+            <p>
+              {Intl.NumberFormat('pt-br', {
+                style: 'currency',
+                currency: 'BRL',
+              }).format(incident.value)}
+            </p>
 
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3" />
-          </button>
-        </li>
-        <li>
-          <strong>CASO:</strong>
-          <p>Nome do caso</p>
-
-          <strong>DESCRIÇÃO:</strong>
-          <p>Descrição do caso</p>
-
-          <strong>VALOR:</strong>
-          <p>
-            {Intl.NumberFormat('pt-br', {
-              style: 'currency',
-              currency: 'BRL',
-            }).format(777)}
-          </p>
-
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3" />
-          </button>
-        </li>{' '}
-        <li>
-          <strong>CASO:</strong>
-          <p>Nome do caso</p>
-
-          <strong>DESCRIÇÃO:</strong>
-          <p>Descrição do caso</p>
-
-          <strong>VALOR:</strong>
-          <p>
-            {Intl.NumberFormat('pt-br', {
-              style: 'currency',
-              currency: 'BRL',
-            }).format(777)}
-          </p>
-
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3" />
-          </button>
-        </li>{' '}
-        <li>
-          <strong>CASO:</strong>
-          <p>Nome do caso</p>
-
-          <strong>DESCRIÇÃO:</strong>
-          <p>Descrição do caso</p>
-
-          <strong>VALOR:</strong>
-          <p>
-            {Intl.NumberFormat('pt-br', {
-              style: 'currency',
-              currency: 'BRL',
-            }).format(777)}
-          </p>
-
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3" />
-          </button>
-        </li>
+            <button
+              type="button"
+              onClick={() => handleDeleteIncident(incident.id)}
+            >
+              <FiTrash2 size={20} color="#a8a8b3" />
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   )
 }
+
+export default Profile
