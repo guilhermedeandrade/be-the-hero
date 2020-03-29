86f87c17a40c4db278377954ddc996b8715a63be
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2020-03-29 00:34:06 +0000

    feat: integrate the logon form with the backend

--- www/src/pages/Logon/index.tsx
+++ www/src/pages/Logon/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useCallback } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import { FiLogIn } from 'react-icons/fi'
+
+import api from '../../services/api'
 
 import './styles.css'
 
@@ -8,15 +10,45 @@ import logoImg from '../../assets/logo.svg'
 import heroesImg from '../../assets/heroes.png'
 
 function Logon() {
+  const [id, setId] = useState('')
+  const history = useHistory()
+
+  const handleChange = useCallback((event) => {
+    setId(event.target.value)
+  }, [])
+
+  const handleLogin = useCallback(
+    async (event) => {
+      event.preventDefault()
+
+      try {
+        const response = await api.post('sessions', { id })
+
+        localStorage.setItem('ongId', id)
+        localStorage.setItem('ongName', response.data.name)
+
+        history.push('/profile')
+      } catch (err) {
+        alert('Falha no login, tente novamente.')
+      }
+    },
+    [id, history]
+  )
+
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="Be the hero" />
 
-        <form action="submit">
+        <form action="submit" onSubmit={handleLogin}>
           <h1>Faça seu logon</h1>
 
-          <input type="text" placeholder="Sua ID" />
+          <input
+            type="text"
+            placeholder="Sua ID"
+            onChange={handleChange}
+            value={id}
+          />
           <button className="button" type="submit">
             Entrar
           </button>
