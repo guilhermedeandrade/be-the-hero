08b961b5c607c443223b8805274c72a29594d899
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2020-03-29 20:09:46 +0100

    feat: add validation middlewares

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import { errors } from 'celebrate'
 
 import routes from './routes'
 
@@ -8,6 +9,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(routes)
+app.use(errors())
 
 const PORT = process.env.PORT ?? 4000
 
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { celebrate, Segments, Joi } from 'celebrate'
 
 import {
   OngController,
@@ -12,12 +13,51 @@ const routes = express.Router()
 routes.post('/sessions', SessionController.create)
 
 routes.get('/ongs', OngController.index)
-routes.post('/ongs', OngController.create)
 
-routes.get('/profile', ProfileController.index)
+routes.post(
+  '/ongs',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.number().required().min(10).max(11),
+      city: Joi.string().required(),
+      uf: Joi.string().required().length(2),
+    }),
+  }),
+  OngController.create
+)
 
-routes.get('/incidents', IncidentController.index)
+routes.get(
+  '/profile',
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required(),
+    }).unknown(),
+  }),
+  ProfileController.index
+)
+
+routes.get(
+  '/incidents',
+  celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+      page: Joi.number(),
+    }),
+  }),
+  IncidentController.index
+)
+
 routes.post('/incidents', IncidentController.create)
-routes.delete('/incidents/:id', IncidentController.delete)
+
+routes.delete(
+  '/incidents/:id',
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().required(),
+    }),
+  }),
+  IncidentController.delete
+)
 
 export default routes
